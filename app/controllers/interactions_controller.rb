efabc127e4ad1dbdc58fea6d3c30fae190db78d1
Josh Muhlfelder <jmuhlfel@berkeley.edu>
2021-01-21 00:50:51 -0800

    add payload to command

--- app/controllers/interactions_controller.rb
+++ app/controllers/interactions_controller.rb
@@ -8,27 +8,25 @@ class InteractionsController < ActionController::API
     json = JSON.parse(request.body.read)
 
     # discord needs to be able to ping the interaction endpoint
-    puts 'asldkfjsadlfjasdlkfjaslkdjf'
-
-    puts json
     if json["type"] == 1
       render json: PING_JSON
       return
     end
+
+    pp json
+    puts '------------------------------------------------'
+    pp params
   end
 
   private
 
   def verify_request
-    puts '-------------------------- verify_request --------------------------'
-    verify_key = Ed25519::VerifyKey.new([ENV["DISCORD_APP_PUBLIC_KEY"]].pack('H*'))
-
     signature = request.headers["X-Signature-Ed25519"]
     timestamp = request.headers["X-Signature-Timestamp"]
-    puts request.body.read
-    puts signature
-    puts timestamp
+
     return head(:unauthorized) unless signature && timestamp
+
+    verify_key = Ed25519::VerifyKey.new([ENV["DISCORD_APP_PUBLIC_KEY"]].pack('H*'))
 
     verify_key.verify([signature].pack('H*'), timestamp + request.body.read.to_s)
   rescue Ed25519::VerifyError
